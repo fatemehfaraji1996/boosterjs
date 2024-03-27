bac4fc7843c5f72641644a2a72d6da6dfc5bb6be
fatemehfaraji1996 <farajifateme1996@gmail.com>
2024-03-27 09:06:15 +0330

    add

---
+++ conditional.js
@@ -0,0 +1,35 @@
+/** @format */
+// ✅
+// using if, else if, else write code for below conditions:
+/*
+    if Shaden is a mentor print out "Hi Shaden, you are a mentor"
+    if Shaden is a student print out "Hi Shaden, you are student"
+    if Shaden is a teacher print out "Hi Shaden, you are a teacher"
+    if she is none of above, print out "Hi Shaden, you should join APS"
+*/
+
+let yourName = 'Shaden';
+let yourRole = 'mentor';
+
+//your code here//
+// let rol = yourName +" "+yourRole
+// console.log(rol);
+
+
+if(yourName == 'Shaden'&& yourRole == 'mentor' ){
+    console.log('Hi Shaden, you are a mentor');
+}else if(yourName == 'Shaden'  &&yourRole === 'student'){
+console.log('Hi Shaden, you are student');
+}else if(yourName == 'Shaden'  &&yourRole === 'teacher'){
+console.log('Hi Shaden, you are a teacher');
+}else{
+    console.log('Hi Shaden, you should join APS');
+}
+
+///
+
+/* 
+EXPECTED RESULT
+---------------
+Hi Shaden, you are a mentor.
+*/
