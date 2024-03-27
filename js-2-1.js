81fac1fdcdaa2276772f0261b2876697ccafca90
fatemehfaraji1996 <farajifateme1996@gmail.com>
2024-03-27 12:12:37 +0330

    add

---
+++ js-2-1.js
@@ -0,0 +1,35 @@
+/**
+ * /*
+ *   Array properties
+ *   ----------------
+ *   Complete the function to test if an array is empty (has no values in it)
+ *✅
+ * @format
+ */
+var numbers = [1, 2, 3];
+var names = [];
+numbers.length
+function isEmpty(arr) {
+  if (arr.length=== 0) {
+    
+    return true; // complete this statement
+  }else{
+    return false
+  }
+
+}
+
+/* 
+  DO NOT EDIT BELOW THIS LINE
+  --------------------------- */
+
+console.log(isEmpty(numbers));
+console.log(isEmpty(names));
+
+/* 
+  EXPECTED RESULT
+  ---------------
+  false
+  true
+*/
+// export default isEmpty;
